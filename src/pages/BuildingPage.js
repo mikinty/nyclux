74cfae9b4f5c87a3b8c9ca7ac717e019a45842ef
Lin Xie <lx68@cornell.edu>
2021-09-27 21:58:53 -0400

    added div

--- src/pages/BuildingPage.js
+++ src/pages/BuildingPage.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { useParams } from 'react-router-dom'
+import {
+  useParams
+} from 'react-router-dom'
 
-import { APARTMENTS } from "../data";
+import {
+  APARTMENTS
+} from "../data";
 
 /**
  * Information page for a building, describing its features.
  */
 
 export default function BuildingPage() {
-  const { name } = useParams();
+  const {
+    name
+  } = useParams();
   const data = APARTMENTS[name];
-  return <div>{JSON.stringify(data)}</div>;
+  return <div >
+    <div>
+      {data.name}
+    </div>
+  </div>;
 }
